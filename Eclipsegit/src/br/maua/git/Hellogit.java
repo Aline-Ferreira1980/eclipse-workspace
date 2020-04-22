827cd04e8db959c5430c5d406343aacf8854699d
Aline&Rodrigo <Aline&Rodrigo@LAPTOP-8CLN27PF>
2020-04-22 15:48:25 -0300

    Voltando para o GitHub

--- Eclipsegit/src/br/maua/git/Hellogit.java
+++ Eclipsegit/src/br/maua/git/Hellogit.java
@@ -6,6 +6,7 @@ public class Hellogit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git ...");
 			System.out.println ("Hello Git - alterando no GitHub");
+				System.out.println("Hello Git - Modificando no Eclipse!!!");
 	}
 
 }
